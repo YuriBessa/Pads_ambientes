d4ee1509eb7a31bdc7f07c174d50f27632d35642
Yuri Bessa <yuri14.levita@gmail.com>
2022-11-04 19:42:56 -0300

    Alterando a lógica e reestilizando o site

--- js/main.js
+++ js/main.js
@@ -1,30 +1,49 @@
 const listaDePads = document.querySelectorAll(".pads")
 
+let padTocando = ""
+
+function buscarPad(seletorAudio) {
+  const padDesejado = document.querySelector(seletorAudio)
+  return padDesejado
+}
+
 function tocaSom(seletorAudio) {
-  const elemento = document.querySelector(seletorAudio)
+  const elemento = buscarPad(seletorAudio)
   if (elemento != null && elemento.localName === "audio") {
     elemento.play()
+    padTocando = elemento
   } else {
-    console.log("Elemento não encontrado ou seletor inválido")
+    alert("Elemento não encontrado ou seletor inválido")
   }
-  console.log(elemento.localName)
+}
+
+function stopPad() {
+  padTocando.pause()
+  padTocando.currentTime = 0
 }
 
 for (let contador = 0; contador < listaDePads.length; contador++) {
   const padSelecionado = listaDePads[contador]
   const nomePad = padSelecionado.classList[1]
   const idAudio = `#som_${nomePad}` //template string
+  const botaoStop = document.getElementById("btn_stop")
 
   padSelecionado.onclick = function () {
-    tocaSom(idAudio)
+    if (padTocando == "") {
+      tocaSom(idAudio)
+    } else {
+      stopPad()
+      tocaSom(idAudio)
+    }
   }
+
+  botaoStop.onclick = () => stopPad()
 
   padSelecionado.onkeydown = function (evento) {
     if (evento.code === "Space" || evento.code === "Enter") {
       padSelecionado.classList.add("ativa")
     }
   }
-
   padSelecionado.onkeyup = function () {
     padSelecionado.classList.remove("ativa")
   }
